cddad79b7fcd5d98c1d9d87d6d0a6ad2d2ff62f4
Jungha Lee <junghalee@Jungha-Lees-MacBook-Pro.local>
2014-07-24 15:57:44 +0200

    Initial Commit

---
+++ C12-Final Lesson one/JLViewController.h
@@ -0,0 +1,13 @@
+//
+//  JLViewController.h
+//  C12-Final Lesson one
+//
+//  Created by Jungha Lee on 7/24/14.
+//  Copyright (c) 2014 Jungha Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLViewController : UIViewController
+
+@end
---
+++ C12-Final Lesson one/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  C12-Final Lesson one
+//
+//  Created by Jungha Lee on 7/24/14.
+//  Copyright (c) 2014 Jungha Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLAppDelegate class]));
+  }
+}
